aed27c0d93c0f601317fe1f24d90fb4f272eeb45
thdiaman <themisdiamantopoulos@hotmail.com>
2015-12-14 18:57:23 +0200

    Put sort functions in a new class to be used as static functions

--- Boa RSSE/src/Miner/Scorer.java
+++ Boa RSSE/src/Miner/Scorer.java
@@ -40,7 +40,7 @@ public class Scorer {
 		System.out.println(Arrays.toString(score));
 		
 		//sort the results
-		scorer.quickSortHighToLow(fileContents,score,0,(score.length-1));
+		SortFunctions.quickSortHighToLow(fileContents,score,0,(score.length-1));
 		System.out.println(Arrays.toString(score));
 				
 		// Create the Final_Files directory if it does not exist
@@ -269,87 +269,7 @@ public class Scorer {
         }
         return (float)ab/(float)(a2+b2-ab);
     }
-	
-	public void quickSortHighToLow(String[] content,float[] arr, int low, int high) {
-		if (arr == null || arr.length == 0)
-			return;
- 
-		if (low >= high)
-			return;
- 
-		// pick the pivot
-		int middle = (high + low) / 2;
-		float pivot = arr[middle];
-		
-		// make left < pivot and right > pivot
-		int i = low, j = high;
-		while (i <= j) {
-			while (arr[i] > pivot) {
-				i++;
-			}
- 
-			while (arr[j] < pivot) {
-				j--;
-			}
- 
-			if (i <= j) {
-				float temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				String temp1 = content[i];
-				content[i] = content[j];
-				content[j] = temp1;
-				i++;
-				j--;
-			}
-		}
-		// recursively sort two sub parts
-		if (low < j)
-			quickSortHighToLow(content,arr, low, j);
- 
-		if (high > i)
-			quickSortHighToLow(content,arr, i, high);
-	}
-	public void quickSortLowToHigh(String[] content,float[] arr, int low, int high) {
-		if (arr == null || arr.length == 0)
-			return;
- 
-		if (low >= high)
-			return;
- 
-		// pick the pivot
-		int middle = (high + low) / 2;
-		float pivot = arr[middle];
-		
-		// make left < pivot and right > pivot
-		int i = low, j = high;
-		while (i <= j) {
-			while (arr[i] < pivot) {
-				i++;
-			}
- 
-			while (arr[j] > pivot) {
-				j--;
-			}
- 
-			if (i <= j) {
-				float temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
-				String temp1 = content[i];
-				content[i] = content[j];
-				content[j] = temp1;
-				i++;
-				j--;
-			}
-		}
-		// recursively sort two sub parts
-		if (low < j)
-			quickSortLowToHigh(content,arr, low, j);
- 
-		if (high > i)
-			quickSortLowToHigh(content,arr, i, high);
-	}
+
 	public void postProcessor(String[] fileContents,float[] score){
 		LOCcounter loccounter = new LOCcounter();
 		float[] loc= new float[fileContents.length];
@@ -369,7 +289,7 @@ public class Scorer {
 				}else break;
 			}
 			if (counter>1){
-				quickSortLowToHigh(fileContents,loc, i, i+counter-1);
+				SortFunctions.quickSortLowToHigh(fileContents,loc, i, i+counter-1);
 				i+=counter-1;
 			}
 		}

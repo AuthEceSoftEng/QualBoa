810c09057d92551ce68e9b90a15d9346f80cc226
thdiaman <themisdiamantopoulos@hotmail.com>
2015-12-15 15:39:00 +0200

    Added a recommender class to be used as the main class of the system

---
+++ Boa RSSE/src/Main/Recommender.java
@@ -0,0 +1,15 @@
+package Main;
+
+public class Recommender {
+
+	public static void main(String[] args) {
+		// TODO: Enter here the main structure of the recommender. Something like the following:
+		// BoaClientDownloader boaDownloader = new BoaClientDownloader(PropertiesHandler.BoaUsername, PropertiesHandler.BoaPassword);
+		// boaDownloader.sendQuery(...)
+		// GitHubFileDownloader gitHubDownloader = new GitHubFileDownloader(PropertiesHandler.GitHubUsername, PropertiesHandler.GitHubPassword);
+		// gitHubDownloader.downloadFiles(...)
+		// Scorer scorer = new Scorer();
+		// Results results = scorer.getRanking(...);
+		// System.out.println(results);
+	}
+}

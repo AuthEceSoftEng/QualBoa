7e4d321e589ca08fe20316694c69b5b2e0c34954
Klearchos <kle-ole@hotmail.com>
2015-12-26 01:45:13 +0200

    The new recommender class to simulate all the system.

--- Boa RSSE/src/Main/Recommender.java
+++ Boa RSSE/src/Main/Recommender.java
@@ -1,15 +1,24 @@
 package Main;
 
+
+import java.io.IOException;
+
+import Downloader.*;
+import edu.iastate.cs.boa.BoaException;
+import Miner.*;
+
 public class Recommender {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception, BoaException, IOException {
 		// TODO: Enter here the main structure of the recommender. Something like the following:
-		// BoaClientDownloader boaDownloader = new BoaClientDownloader(PropertiesHandler.BoaUsername, PropertiesHandler.BoaPassword);
-		// boaDownloader.sendQuery(...)
-		// GitHubFileDownloader gitHubDownloader = new GitHubFileDownloader(PropertiesHandler.GitHubUsername, PropertiesHandler.GitHubPassword);
-		// gitHubDownloader.downloadFiles(...)
-		// Scorer scorer = new Scorer();
-		// Results results = scorer.getRanking(...);
-		// System.out.println(results);
+		BoaClientDownloader boaDownloader = new BoaClientDownloader(PropertiesHandler.BoaUsername, PropertiesHandler.BoaPassword);
+		boaDownloader.runQuery("input.java");
+		GitHubFileDownloader gitHubDownloader = new GitHubFileDownloader(PropertiesHandler.GitHubUsername, PropertiesHandler.GitHubPassword);
+		gitHubDownloader.downloadFiles("Boa_output.txt");
+		Scorer scorer = new Scorer();
+		String[] results = scorer.getResults();
+		//print top 10
+		System.out.println("Top 10 recommended results: \n");
+		for (int i=0;i<10;i++)System.out.println("\n"+(i+1)+"."+"\n\n"+results[i]);
 	}
 }

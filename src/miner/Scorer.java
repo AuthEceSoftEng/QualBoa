9a00cdea1a177114a0ee207f887d62be65da5829
thdiaman <themisdiamantopoulos@hotmail.com>
2016-06-08 14:25:42 +0300

    Clean up the code of the functional scorer

--- src/miner/Scorer.java
+++ src/miner/Scorer.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import database.DownloadedFile;
@@ -142,6 +143,17 @@ public class Scorer {
 		return score;
 	}
 
+	class PairWithScore {
+		int index1;
+		int index2;
+		double score;
+		public PairWithScore(int index1, int index2, double score) {
+			this.index1 = index1;
+			this.index2 = index2;
+			this.score = score;
+		}
+	}
+
 	public double[] methodsScore(Signature inputSignature, Signature outputSignature) {
 		String[] inputMethodsName = inputSignature.getMethodNames().replace("\"", "").split(",");
 		String[] inputMethodsType = inputSignature.getMethodTypes().replace("\"", "").split(",");
@@ -149,73 +161,100 @@ public class Scorer {
 		String[] outputMethodsType = outputSignature.getMethodTypes().replace("\"", "").split(",");
 		String[][] inputMethodArgs = inputSignature.getMethodArgs();
 		String[][] outputMethodArgs = outputSignature.getMethodArgs();
-		String[] hasBlock = outputSignature.getBlock().split(",");
+		ArrayList<PairWithScore> methodPairs = new ArrayList<Scorer.PairWithScore>();
 
-		double[] methodScore = new double[inputMethodsName.length];
-
-		// definition of methodVector = [nameScore, typeScore, argScore]
-		double[] methodVector = { 0,0,0 };
-		double[] defaultVector = { 1,1,1 };
-
-		for (int i = 0; i < inputMethodsName.length; i++) {
-			methodScore[i] = 0;
-		}
-		// find the max method score for each method
 		for (int i = 0; i < inputMethodsName.length; i++) {
 			for (int j = 0; j < outputMethodsName.length; j++) {
-				double jaccard = 0;
-				double tanimoto = 0;
-				for (int k = 0; k < methodVector.length; k++)
-					methodVector[k] = 0;
-
-				String[] inputMethodTokens = tokenizeString(inputMethodsName[i]);
-				String[] outputMethodTokens = tokenizeString(outputMethodsName[j]);
-
-				jaccard = JaccardCoefficient.similarity(inputMethodTokens, outputMethodTokens);
-				if (hasBlock[j].trim().equals("yes") && jaccard > 0) {
-					methodVector[0] = jaccard;
-					if (inputMethodsType[i].trim().equals(outputMethodsType[j]))
-						methodVector[1] = 1;
-					methodVector[2] = calculateArgsScore(inputMethodArgs,i,outputMethodArgs,j);
-				}
-				tanimoto = TanimotoCoefficient.similarity(methodVector, defaultVector);
-				if (tanimoto > methodScore[i]) {
-					methodScore[i] = tanimoto;
-				}
+				double score = calculateMethodsScore(inputMethodsName[i], outputMethodsName[j], inputMethodsType[i],
+						outputMethodsType[j], inputMethodArgs[i], outputMethodArgs[j]);
+				methodPairs.add(new PairWithScore(i, j, score));
 			}
 		}
+		Collections.sort(methodPairs, new Comparator<PairWithScore>(){
+			public int compare(PairWithScore o1, PairWithScore o2) {
+				if (o1.score == o2.score)
+					return 0;
+				return o1.score < o2.score ? 1 : -1;
+			}
+		});
+
+		HashSet<Integer> matchedList1 = new HashSet<Integer>(); 
+ 		HashSet<Integer> matchedList2 = new HashSet<Integer>(); 
+ 		List<PairWithScore> selectedMethodPairs = new ArrayList<PairWithScore>(); 
+ 		for (PairWithScore methodPair : methodPairs) { 
+			if (!(matchedList1.contains(methodPair.index1) || matchedList2.contains(methodPair.index2))) {
+ 				selectedMethodPairs.add(methodPair); 
+ 				matchedList1.add(methodPair.index1); 
+ 				matchedList2.add(methodPair.index2); 
+ 			} 
+ 		} 
+		double[] methodScore = new double[inputMethodsName.length];
+ 		for (PairWithScore pairWithScore : selectedMethodPairs) {
+			methodScore[pairWithScore.index1] = pairWithScore.score;
+		}
+
 		// Test the results
 		// System.out.println("method score: " + Arrays.toString(methodScore));
 
 		return methodScore;
 	}
 
-	private double calculateArgsScore(String[][] inputMethodArgs, int a, String[][] outputMethodArgs, int b) {
-		int counter = 0;
-		String[][] inputArgs = inputMethodArgs;
-		String[][] outputArgs = outputMethodArgs;
+	private double calculateMethodsScore(String inputMethodName, String outputMethodName, String inputMethodType,
+			String outputMethodType, String[] inputMethodArgs, String[] outputMethodArgs) {
+		double[] methodVector = new double[2 + inputMethodArgs.length];
+		double[] defaultVector = new double[2 + inputMethodArgs.length];
+		Arrays.fill(defaultVector, 1);
+		for (int k = 0; k < methodVector.length; k++)
+			methodVector[k] = 0;
 
-		for (int i = 0; i < inputArgs[a].length; i++) {
-			for (int j = 0; j < outputArgs[b].length; j++) {
-				if (inputArgs[a][i].equals(outputArgs[b][j])){
-					counter++;
-					outputArgs[b][j] = null;
-				}
+		String[] inputMethodTokens = tokenizeString(inputMethodName);
+		String[] outputMethodTokens = tokenizeString(outputMethodName);
+
+		methodVector[0] = JaccardCoefficient.similarity(inputMethodTokens, outputMethodTokens);
+		if (inputMethodType.trim().toLowerCase().equals(outputMethodType.trim().toLowerCase()))
+			methodVector[1] = 1;
+		else
+			methodVector[1] = 0;
+		if (inputMethodArgs.length > 0){
+			double[] argsScore = calculateArgsScore(inputMethodArgs, outputMethodArgs);
+			for (int i = 0; i < argsScore.length; i++)
+				methodVector[i + 2] = argsScore[i];
+		}
+		return TanimotoCoefficient.similarity(methodVector, defaultVector);
+	}
+
+	private double[] calculateArgsScore(String[] inputArgs, String[] outputArgs) {
+		ArrayList<PairWithScore> argPairs = new ArrayList<Scorer.PairWithScore>();
+		for (int i = 0; i < inputArgs.length; i++) {
+			for (int j = 0; j < outputArgs.length; j++) {
+				String[] inputArgTokens = tokenizeString(inputArgs[i]);
+				String[] outputArgTokens = tokenizeString(outputArgs[j]);
+				double score = JaccardCoefficient.similarity(inputArgTokens, outputArgTokens);
+				argPairs.add(new PairWithScore(i, j, score));
 			}
 		}
-
-		double score;
-		if (inputArgs[a].length < outputArgs[b].length) {
-			score = (double) counter / outputArgs[b].length;
-		} else if (inputArgs[a].length > outputArgs[b].length) {
-			score = (double) counter / inputArgs[a].length;
-		} else {
-			if (inputArgs[a].length == 0)
-				score = 1;
-			else
-				score = (double) counter / inputArgs[a].length;
+		Collections.sort(argPairs, new Comparator<PairWithScore>(){
+			public int compare(PairWithScore o1, PairWithScore o2) {
+				if (o1.score == o2.score)
+					return 0;
+				return o1.score < o2.score ? 1 : -1;
+			}
+		});
+		HashSet<Integer> matchedList1 = new HashSet<Integer>(); 
+ 		HashSet<Integer> matchedList2 = new HashSet<Integer>(); 
+ 		List<PairWithScore> selectedArgPairs = new ArrayList<PairWithScore>(); 
+ 		for (PairWithScore argPair : argPairs) { 
+			if (!(matchedList1.contains(argPair.index1) || matchedList2.contains(argPair.index2))) {
+ 				selectedArgPairs.add(argPair); 
+ 				matchedList1.add(argPair.index1); 
+ 				matchedList2.add(argPair.index2); 
+ 			} 
+ 		} 
+		double[] argScore = new double[inputArgs.length];
+ 		for (PairWithScore pairWithScore : selectedArgPairs) {
+ 			argScore[pairWithScore.index1] = pairWithScore.score;
 		}
-		return score;
+		return argScore;
 	}
 
 	public String[] tokenizeString(String text) {
